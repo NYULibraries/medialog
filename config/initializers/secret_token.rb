23c2aa8f356efa8a09d409090fb6394e3bcc65d2
Joseph Pawletko <jgpawletko@gmail.com>
2014-02-10 11:43:17 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medialog::Application.config.secret_key_base = '0f3b7ee03964420d9bb06d05d30af6f8f8a75021ed711313d96f128aaef65d249d1204b6488ee62f93e0d21fc589c92b6a8ab2fff878c6344b937c152ff086a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Medialog::Application.config.session_store :cookie_store, key: '_medialog_session'
