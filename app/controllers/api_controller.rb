5c4fc8f1feac8f61dcb96e286d6aebc2012c6919
Don Mennerich <don.mennerich@nyu.edu>
2016-03-09 16:06:34 -0500

    adding api controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,9 @@
+module Api
+  module V0
+  	class ApiController < ApplicationController
+      def accession
+  	    "hello, there"
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -40,5 +40,13 @@ Medialog::Application.routes.draw do
   get 'reports/format' => 'reports#format'
 
   get 'reports/format/:type' => 'reports#type'  
-  
+
+  namespace :api, constraints: { format: 'json' } do
+    namespace :v0 do
+      get :accession
+      get :repository
+      get :collection
+      get :media 
+    end
+  end  
 end
