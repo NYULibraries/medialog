1d2fc9fb5dff0bd6c5f5d7c82e39f890531b601a
Donald Mennerich <don.mennerich@nyu.edu>
2016-04-12 07:34:25 +0900

    upating summary functions in  api

--- app/controllers/api/v0_controller.rb
+++ app/controllers/api/v0_controller.rb
@@ -4,21 +4,8 @@ class Api::V0Controller < ApplicationController
     mlog_entries = MlogEntry.where("accession_id = ?", params["id"])
     accession =  Accession.find(params["id"])
     collection = Collection.find(accession.collection_id)
-    formats = Hash.new
-    entries = Hash.new
-    
-    mlog_entries.each do |entry|
-      entries[entry.id] = entry.media_id
-      format =MLOG_VOCAB["mediatypes"][entry["mediatype"]]
-      if formats.has_key? format
-      	prev = formats[format] 
-      	formats[format] = { "qty" => prev["qty"] + 1, "unit" => prev["unit"], "size" => prev["size"] + entry["stock_size"].to_f } 
-      else 
-      	formats[format] = { "qty" => 1, "unit" => entry["stock_unit"], "size" => entry["stock_size"].to_f }
-      end
-    end
-    
-    render :json => {"accession" => accession, "collection" => collection, "media_formats" => formats, "inventory" => entries }
+    stuff = getTypes(mlog_entries)
+    render :json => {"accession" => accession, "collection" => collection, "media_formats" => stuff["formats"], "inventory" => stuff["entries"] }
   end
 
   def mlog_entry
@@ -26,32 +13,34 @@ class Api::V0Controller < ApplicationController
   end
 
   def collection
-
-    collection_rec = Collection.find(params["id"])
-    accession_recs = Accession.where("collection_id = ?", params["id"])
-    mlog_entries = MlogEntry.where("collection_id = ?", params["id"])
-    formats = Hash.new
-    entries = Hash.new
+    collection = Collection.find(params["id"])
+    accession_recs = Accession.where("collection_id = ?", collection.id)
+    stuff = getTypes(MlogEntry.where("collection_id = ?", collection.id))
+    
     accessions = Hash.new
-
     accession_recs.each do |accession|
       accessions[accession.id] = accession.accession_num
     end
-
-
-
-    mlog_entries.each do |entry|
-      entries[entry.id] = entry.media_id
-      format =MLOG_VOCAB["mediatypes"][entry["mediatype"]]
-      if formats.has_key? format
-        prev = formats[format] 
-        formats[format] = { "qty" => prev["qty"] + 1, "unit" => prev["unit"], "size" => prev["size"] + entry["stock_size"].to_f } 
-      else 
-        formats[format] = { "qty" => 1, "unit" => entry["stock_unit"], "size" => entry["stock_size"].to_f }
-      end
-    end
-  
-
-    render :json => {"collection" => collection_rec, "acccessions" => accessions, "media_formats" => formats, "inventory" => entries}
+    render :json => {"collection" => collection, "acccessions" => accessions, "media_formats" => stuff["formats"], "inventory" => stuff["entries"]}
   end
+
+  private
+    def getTypes(mlog_entries)
+      entries = Hash.new
+      formats = Hash.new
+      stuff = Hash.new
+      mlog_entries.each do |entry|
+        entries[entry.id] = entry.media_id
+        format =MLOG_VOCAB["mediatypes"][entry["mediatype"]]
+        if formats.has_key? format
+          prev = formats[format] 
+          formats[format] = { "qty" => prev["qty"] + 1, "unit" => prev["unit"], "size" => prev["size"] + entry["stock_size"].to_f } 
+        else 
+          formats[format] = { "qty" => 1, "unit" => entry["stock_unit"], "size" => entry["stock_size"].to_f }
+        end
+      end
+      stuff["entries"] = entries
+      stuff["formats"] = formats
+      stuff
+    end
 end
